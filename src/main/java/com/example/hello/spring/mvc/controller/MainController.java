d20f8e30ceae05e00f783fc5c686b02c0a8d981a
Gabriele02 <gagag@g,ail.com>
2024-01-03 17:22:32 +0100

    setup

--- src/main/java/com/example/hello/spring/mvc/controller/MainController.java
+++ src/main/java/com/example/hello/spring/mvc/controller/MainController.java
@@ -1,5 +1,22 @@
 package com.example.hello.spring.mvc.controller;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Random;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+     @GetMapping
+    public String dado(@RequestParam int numberFaces, Model model){
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(1, (numberFaces + 1));
+        model.addAttribute("randomNumber", randomNumber);
+        return "dado";
+    }
 }
