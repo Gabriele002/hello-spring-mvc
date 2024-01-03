5122114cc8b34742d3f679b5d7d6f2dd66ebb01c
Gabriele02 <gagag@g,ail.com>
2024-01-03 15:02:01 +0100

    work space

---
+++ src/test/java/com/example/hello/spring/mvc/HelloSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello.spring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
